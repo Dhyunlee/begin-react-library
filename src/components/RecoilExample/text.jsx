ec499b1c95370db10934478acdbf5bdf20356143
Donghyun Lee <skyblue5030@naver.com>
2022-05-16 17:25:44 +0900

    recoil - selector와 useRecoilValue 사용하기

--- src/components/RecoilExample/text.jsx
+++ src/components/RecoilExample/text.jsx
@@ -1,14 +1,21 @@
-import { useRecoilState } from 'recoil';
-import { fontSizeState } from './store';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { fontSizeLabelState, fontSizeState } from './store';
 
 function Text() {
   const [fontSize, setFontSize] =
     useRecoilState(fontSizeState);
+
+  const fontSizeLabel = useRecoilValue(fontSizeLabelState);
   return (
-    <p style={{ fontSize }}>
-      텍스트의 크기가 달라지고 있나요? <br />
-      현재 글자 크기:{fontSize}
-    </p>
+    <div>
+      <p style={{ fontSize }}>
+        텍스트의 크기가 달라지고 있나요? <br />
+      </p>
+      <p>
+        현재 폰트 크기:
+        <span style={{ fontSize }}>{fontSizeLabel}</span>
+      </p>
+    </div>
   );
 }
 
